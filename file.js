c09ccaea2f94f021e58e2c9d47bc6e6f2fe386ef
AyushKumar015 <kumar.ayush15102002@gmail.com>
2024-01-16 19:59:47 +0530

    file added

---
+++ file.js
@@ -0,0 +1,210 @@
+
+var b=0
+let player1="O"
+let player2="X"
+
+function call0()
+{
+    if(document.getElementsById("0").innerHTML== " ")
+    {
+        if(b%2==0)
+        {
+            document.getElementById("0").innerHTML="X"
+        }
+         else{
+            document.getElementById("0").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call1()
+{
+    if(document.getElementsById("1").innerHTML=="")
+    {
+        if(b%2==0)
+        {
+            document.getElementById("1").innerHTML="X"
+        }
+         else{
+            document.getElementById("1").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call2()
+{
+    if(document.getElementsById("2").innerHTML=="")
+    {
+        if(b%2==0)
+        {
+            document.getElementById("2").innerHTML="X"
+        }
+         else{
+            document.getElementById("2").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call3()
+{
+    if(document.getElementsById("3").innerHTML=="")
+    {
+        if(b%2==0)
+        {
+            document.getElementById("3").innerHTML="X"
+        }
+         else{
+            document.getElementById("3").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call4()
+{
+    if(document.getElementsById("4").innerHTML=="")
+    {
+        if(b%2==0)
+        {
+             document.getElementById("4").innerHTML="X"
+        }
+         else{
+            document.getElementById("4").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call5()
+{
+    if(document.getElementsById("5").innerHTML=="")
+    {
+        if(b%2==0)
+        {
+             document.getElementById("5").innerHTML="X"
+        }
+         else{
+            document.getElementById("5").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call6()
+{
+    if(document.getElementsById('6').innerHTML=="")
+    {
+        if(b%2==0)
+        {
+             document.getElementById("6").innerHTML="X"
+        }
+         else{
+            document.getElementById("6").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call7()
+{
+    if(document.getElementsById('7').innerHTML=="")
+    {
+        if(b%2==0)
+        {
+             document.getElementById("7").innerHTML="X"
+        }
+         else{
+            document.getElementById("7").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+function call8()
+{
+    if(document.getElementsById('8').innerHTML=="")
+    {
+        if(b%2==0)
+        {
+             document.getElementById("8").innerHTML="X"
+        }
+         else{
+            document.getElementById("8").innerHTML="O"
+        }
+        b++
+        //winning()
+    }
+    else
+    {
+        alert('space already filled')
+    }
+}
+
+// function winning()
+// {
+//     let c=document.getElementsByTagName('p')
+//     if (b>4)
+//     {
+//         if (((c[0].innerHTML==c[1].innerHTML) && (c[0].innerHTML==c[2].innerHTML)) || ((c[3].innerHTML==c[4].innerHTML) && (c[3].innerHTML==c[5].innerHTML)) || ((c[6].innerHTML==c[8].innerHTML) && (c[6].innerHTML==c[7].innerHTML)) || ((c[0].innerHTML==c[3].innerHTML) && (c[0].innerHTML==c[6].innerHTML)) || ((c[1].innerHTML==c[4].innerHTML) && (c[1].innerHTML==c[7].innerHTML)) || ((c[2].innerHTML==c[5].innerHTML) && (c[2].innerHTML==c[8].innerHTML)) || ((c[0].innerHTML==c[4].innerHTML) && (c[0].innerHTML==c[8].innerHTML)) || ((c[2].innerHTML==c[4].innerHTML) && (c[2].innerHTML==c[6].innerHTML)))
+//           {
+//               document.getElementById('win').innerHTML="winner is" +" " + c[0].innerHTML
+//               for(let i=0;i<9;i++)
+//                {
+//                   c[i].innerHTML=''
+//                }
+       
+//           }
+//     }
+// }
+
+function reset()
+{
+    let c=document.getElementsByTagName('p')
+    for(let i=0;i<10;i++)
+        {
+            c[i].innerHTML=''
+        }
+}
